7b3bde9d29060dc461a9a7523f9de1687e73e421
Gyuwon Yi <gyuwon@live.com>
2021-02-27 00:23:03 +0900

    Variance 1.0

---
+++ Variance/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Variance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = args.Length;
+
+            double[] source = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                source[i] = double.Parse(args[i]);
+            }
+
+            double sum = 0.0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += source[i];
+            }
+
+            double mean = sum / n;
+
+            double sumOfSquares = 0.0;
+            for (int i = 0; i < n; i++)
+            {
+                sumOfSquares += (source[i] - mean) * (source[i] - mean);
+            }
+
+            double variance = sumOfSquares / (n - 1);
+
+            Console.WriteLine($"분산: {variance}");
+        }
+    }
+}
