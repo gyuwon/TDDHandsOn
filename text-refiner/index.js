4f48e216b04eff9a2723770bd23e9fca08e55be9
Gyuwon Yi <gyuwon@live.com>
2021-03-01 11:17:18 +0900

    Enhance refineText function in test-first manner

--- text-refiner/index.js
+++ text-refiner/index.js
@@ -1,5 +1,22 @@
-function refineText(s) {
-  return s.replace("    ", " ").replace("  ", " ");
+function refineText(s, options) {
+  s = s
+    .replace("    ", " ")
+    .replace("\t ", " ")
+    .replace(" \t", " ")
+    .replace("  ", " ")
+    .replace("  ", " ")
+    .replace("  ", " ")
+    .replace("mockist", "*******")
+    .replace("purist", "******");
+
+  if (options) {
+    for (const bannedWord of options.bannedWords) {
+      const mask = "*".repeat(bannedWord.length);
+      s = s.replace(bannedWord, mask);
+    }
+  }
+
+  return s;
 }
 
 module.exports = refineText;
--- text-refiner/index.test.js
+++ text-refiner/index.test.js
@@ -1,11 +1,52 @@
+const faker = require("faker");
 const sut = require("./index");
 
 test.each`
-  source              | expected
-  ${"hello  world"}   | ${"hello world"}
-  ${"hello   world"}  | ${"hello world"}
-  ${"hello    world"} | ${"hello world"}
+  source                 | expected
+  ${"hello  world"}      | ${"hello world"}
+  ${"hello   world"}     | ${"hello world"}
+  ${"hello    world"}    | ${"hello world"}
+  ${"hello     world"}   | ${"hello world"}
+  ${"hello      world"}  | ${"hello world"}
+  ${"hello       world"} | ${"hello world"}
 `('sut transforms "$source" to "$expected"', ({ source, expected }) => {
   const actual = sut(source);
   expect(actual).toBe(expected);
+});
+
+test.each`
+  source             | expected
+  ${"hello\t world"} | ${"hello world"}
+  ${"hello \tworld"} | ${"hello world"}
+`(
+  'sut transforms "$source" that contains \'\\t\' to "$expected"',
+  ({ source, expected }) => {
+    const actual = sut(source);
+    expect(actual).toBe(expected);
+  }
+);
+
+test.each`
+  source             | bannedWords              | expected
+  ${"hello mockist"} | ${["mockist", "purist"]} | ${"hello *******"}
+  ${"hello purist"}  | ${["mockist", "purist"]} | ${"hello ******"}
+`(
+  'sut transforms "$source" to "$expected"',
+  ({ source, bannedWords, expected }) => {
+    const options = { bannedWords };
+    const actual = sut(source, options);
+    expect(actual).toBe(expected);
+  }
+);
+
+describe("given banned word", () => {
+  const bannedWord = faker.lorem.word();
+  const masked = "*".repeat(bannedWord.length);
+  const source = `hello ${bannedWord}`;
+  const options = { bannedWords: [bannedWord] };
+
+  test(`"${bannedWord}" when invoke sut then it returns "hello ${masked}"`, () => {
+    const actual = sut(source, options);
+    expect(actual).toBe(`hello ${masked}`);
+  });
 });
