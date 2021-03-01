64f88fe0461585dd0ec234b3af169334c1ee4f97
Gyuwon Yi <gyuwon@live.com>
2021-03-01 12:20:57 +0900

    Implement multiplayer mode

--- NumberGuessing/console-app-model/src/main/java/numberguessing/console/AppModel.java
+++ NumberGuessing/console-app-model/src/main/java/numberguessing/console/AppModel.java
@@ -1,5 +1,7 @@
 package numberguessing.console;
 
+import java.util.Arrays;
+
 import numberguessing.PositiveIntegerGenerator;
 
 public final class AppModel {
@@ -42,24 +44,61 @@ public final class AppModel {
             println("Single player game");
             println("I'm thinking of a number between 1 and 100.");
             print("Enter your guess: ");
-            return getPlayProcessor(answer, 1);
+            return getSinglePlayerGameProcessor(answer, 1);
+        } else if (input.startsWith("2")) {
+            println("Multiplayer game");
+            print("Enter player names separated with commas: ");
+            return getPlayerNamesProcessor();
         } else {
             completed = true;
             return null;
         }
     }
 
-    private Processor getPlayProcessor(int answer, int tries) {
+    private Processor getPlayerNamesProcessor() {
+        return input -> {
+            Object[] players = Arrays.stream(input.split(",")).map(String::trim).toArray();
+            int answer = generator.generateLessThanOrEqualToHundread();
+            println("I'm thinking of a number between 1 and 100.");
+            print("Enter " + players[0] + "'s guess: ");
+            return getMultiplayerGameProcessor(players, answer, 1);
+        };
+    }
+
+    private Processor getMultiplayerGameProcessor(Object[] players, int answer, int tries) {
+        return input -> {
+            int guess = Integer.parseInt(input);
+
+            Object currentPlayer = players[(tries - 1) % players.length];
+            Object nextPlayer = players[tries % players.length];
+
+            if (guess < answer) {
+                println(currentPlayer + "'s guess is too low.");
+                print("Enter " + nextPlayer + "'s guess: ");
+                return getMultiplayerGameProcessor(players, answer, tries + 1);
+            } else if (guess > answer) {
+                println(currentPlayer + "'s guess is too high.");
+                print("Enter " + nextPlayer + "'s guess: ");
+                return getMultiplayerGameProcessor(players, answer, tries + 1);
+            } else {
+                println("Correct! " + currentPlayer + " wins.");
+                print(selectModeMessage);
+                return this::processSelection;
+            }
+        };
+    }
+
+    private Processor getSinglePlayerGameProcessor(int answer, int tries) {
         return input -> {
             int guess = Integer.parseInt(input);
             if (guess < answer) {
                 println("Your guess is too low.");
                 print("Enter your guess: ");
-                return getPlayProcessor(answer, tries + 1);
+                return getSinglePlayerGameProcessor(answer, tries + 1);
             } else if (guess > answer) {
                 println("Your guess is too high.");
                 print("Enter your guess: ");
-                return getPlayProcessor(answer, tries + 1);
+                return getSinglePlayerGameProcessor(answer, tries + 1);
             } else {
                 println("Correct! " + tries + (tries == 1 ? " guess." : " guesses."));
                 print(selectModeMessage);
--- NumberGuessing/console-app-model/src/test/java/numberguessing/console/AppModel_specs.java
+++ NumberGuessing/console-app-model/src/test/java/numberguessing/console/AppModel_specs.java
@@ -163,4 +163,177 @@ public class AppModel_specs {
         assertThat(actual).startsWith("Correct! ");
     }
 
+    @Test
+    void sut_correctly_prints_multiplayer_game_setup_message() {
+        var sut = new AppModel(new PositiveIntegerGeneratorStub(50));
+        sut.flushOutput();
+        sut.processInput("2");
+
+        String actual = sut.flushOutput();
+
+        assertThat(actual).isEqualTo("Multiplayer game" + NEW_LINE + "Enter player names separated with commas: ");
+    }
+
+    @Test
+    void sut_correctly_prints_multiplayer_game_start_message() {
+        var sut = new AppModel(new PositiveIntegerGeneratorStub(50));
+        sut.processInput("2");
+        sut.flushOutput();
+        sut.processInput("Foo, Bar");
+
+        String actual = sut.flushOutput();
+
+        assertThat(actual).startsWith("I'm thinking of a number between 1 and 100.");
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "Foo, Bar, Baz", "Bar, Baz, Foo", "Baz, Foo, Bar" })
+    void sut_correctly_prompts_first_player_name(String player1, String player2, String player3) {
+        var sut = new AppModel(new PositiveIntegerGeneratorStub(50));
+        sut.processInput("2");
+        sut.flushOutput();
+        sut.processInput(String.join(", ", player1, player2, player3));
+
+        String actual = sut.flushOutput();
+
+        assertThat(actual).endsWith("Enter " + player1 + "'s guess: ");
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "Foo, Bar, Baz", "Bar, Baz, Foo", "Baz, Foo, Bar" })
+    void sut_correctly_prompts_second_player_name(String player1, String player2, String player3) {
+        var sut = new AppModel(new PositiveIntegerGeneratorStub(50));
+        sut.processInput("2");
+        sut.processInput(String.join(", ", player1, player2, player3));
+        sut.flushOutput();
+        sut.processInput("10");
+
+        String actual = sut.flushOutput();
+
+        assertThat(actual).endsWith("Enter " + player2 + "'s guess: ");
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "Foo, Bar, Baz", "Bar, Baz, Foo", "Baz, Foo, Bar" })
+    void sut_correctly_prompts_third_player_name(String player1, String player2, String player3) {
+        var sut = new AppModel(new PositiveIntegerGeneratorStub(50));
+        sut.processInput("2");
+        sut.processInput(String.join(", ", player1, player2, player3));
+        sut.processInput("90");
+        sut.flushOutput();
+        sut.processInput("90");
+
+        String actual = sut.flushOutput();
+
+        assertThat(actual).endsWith("Enter " + player3 + "'s guess: ");
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "Foo, Bar, Baz", "Bar, Baz, Foo", "Baz, Foo, Bar" })
+    void sut_correctly_rounds_players(String player1, String player2, String player3) {
+        var sut = new AppModel(new PositiveIntegerGeneratorStub(50));
+        sut.processInput("2");
+        sut.processInput(String.join(", ", player1, player2, player3));
+        sut.processInput("10");
+        sut.processInput("10");
+        sut.flushOutput();
+        sut.processInput("10");
+
+        String actual = sut.flushOutput();
+
+        assertThat(actual).endsWith("Enter " + player1 + "'s guess: ");
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "50, 40, 1, Foo", "30, 29, 2, Bar" })
+    void sut_correctly_prints_too_low_message_in_multiplayer_game(int answer, int guess, int fails, String lastPlayer) {
+        var sut = new AppModel(new PositiveIntegerGeneratorStub(answer));
+        sut.processInput("2");
+        sut.processInput("Foo, Bar, Baz");
+        for (int i = 0; i < fails - 1; i++)
+            sut.processInput(Integer.toString(guess));
+        sut.flushOutput();
+        sut.processInput(Integer.toString(guess));
+
+        String actual = sut.flushOutput();
+
+        assertThat(actual).startsWith(lastPlayer + "'s guess is too low." + NEW_LINE);
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "50, 60, 1, Foo", "9, 81, 2, Bar" })
+    void sut_correctly_prints_too_high_message_in_multiplayer_game(int answer, int guess, int fails,
+            String lastPlayer) {
+        var sut = new AppModel(new PositiveIntegerGeneratorStub(answer));
+        sut.processInput("2");
+        sut.processInput("Foo, Bar, Baz");
+        for (int i = 0; i < fails - 1; i++)
+            sut.processInput(Integer.toString(guess));
+        sut.flushOutput();
+        sut.processInput(Integer.toString(guess));
+
+        String actual = sut.flushOutput();
+
+        assertThat(actual).contains(lastPlayer + "'s guess is too high." + NEW_LINE);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = { 1, 10, 100 })
+    void sut_correctly_prints_correct_message_in_multiplayer_game(int answer) {
+        var sut = new AppModel(new PositiveIntegerGeneratorStub(answer));
+        sut.processInput("2");
+        sut.processInput("Foo, Bar, Baz");
+        sut.flushOutput();
+        int guess = answer;
+        sut.processInput(Integer.toString(guess));
+
+        String actual = sut.flushOutput();
+
+        assertThat(actual).startsWith("Correct! ");
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "0, Foo", "1, Bar", "2, Baz", "99, Foo", "100, Bar" })
+    void sut_correctly_prints_winner_if_multiplayer_game_finished(int fails, String winner) {
+        var sut = new AppModel(new PositiveIntegerGeneratorStub(50));
+        sut.processInput("2");
+        sut.processInput("Foo, Bar, Baz");
+        for (int i = 0; i < fails; i++)
+            sut.processInput("30");
+        sut.flushOutput();
+        sut.processInput("50");
+
+        String actual = sut.flushOutput();
+
+        assertThat(actual).contains(winner + " wins." + NEW_LINE);
+    }
+
+    @Test
+    void sut_prints_select_mode_message_if_multiplayer_game_finished() {
+        var sut = new AppModel(new PositiveIntegerGeneratorStub(50));
+        sut.processInput("2");
+        sut.processInput("Foo, Bar, Baz");
+        sut.flushOutput();
+        sut.processInput("50");
+
+        String actual = sut.flushOutput();
+
+        assertThat(actual).endsWith("1: Single player game" + NEW_LINE + "2: Multiplayer game" + NEW_LINE + "3: Exit"
+                + NEW_LINE + "Enter selection: ");
+    }
+
+    @Test
+    void sut_returns_to_mode_selection_if_multiplayer_game_finished() {
+        var sut = new AppModel(new PositiveIntegerGeneratorStub(50));
+
+        sut.processInput("2");
+        sut.processInput("Foo, Bar, Baz");
+        sut.processInput("20");
+        sut.processInput("50");
+        sut.processInput("3");
+
+        boolean actual = sut.isCompleted();
+        assertTrue(actual);
+    }
+
 }
