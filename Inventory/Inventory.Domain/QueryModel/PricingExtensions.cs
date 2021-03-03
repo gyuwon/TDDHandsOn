09d5e61a4c79f86def48d07da7892a8c38365417
Gyuwon Yi <gyuwon@live.com>
2021-03-03 11:55:30 +0900

    Calculate selling price

---
+++ Inventory/Inventory.Domain/QueryModel/PricingExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace Inventory.QueryModel
+{
+    internal static class PricingExtensions
+    {
+        public static decimal CalculateSellingPrice(this Pricing pricing)
+            => pricing.ListPrice - pricing.Discount;
+    }
+}
